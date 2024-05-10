835a3839ade1fb77a0cee61bd4df36c3b14d8740
kingpinlubiku <lubiku@proton.me>
2024-05-10 12:27:01 +0200

    Created initial structure for API routing

--- app/routes/apiroutes.py
+++ app/routes/apiroutes.py
@@ -4,6 +4,35 @@ from model.data_handler  import delete_data
 
 api_routes = Blueprint('api_routes', __name__)
 
+# Create an item
+@api_routes.route('/api/items', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    if data:
+        return jsonify(data), 200
+    else:
+        return jsonify({'message': 'Failed to create an item'}), 400
+
+# Read all items
+@api_routes.route('/api/items', methods=['GET'])
+def read_items():
+    return jsonify({'message': 'Read all items'}), 200
+
+# Read an item
+@api_routes.route('/api/item/<int:item_id>', methods=['GET'])
+def read_item(item_id):
+    return jsonify({'message': 'Read an item'}), 200
+
+# Update an item
+@api_routes.route('/api/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    data = request.get_json()
+    if data:
+        return jsonify(data), 200
+    else:
+        return jsonify({'message': 'Failed to update an item'}), 400
+
+# Delete an item
 @api_routes.route('/api/items/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
     is_deleted = delete_data(item_id)
